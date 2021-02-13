b3e4c114d9a430f93854bf76c13c4eded29818c1
Emre Sen <maemresen@yazilim.vip>
2021-02-13 22:07:19 +0300

    I(thymeleaf-crud): update

--- 01-spring-boot-basics/basics-bean-scopes/src/main/kotlin/vip/yazilim/basics/bean_scopes/AppBeanScopes.kt
+++ 01-spring-boot-basics/basics-bean-scopes/src/main/kotlin/vip/yazilim/basics/bean_scopes/AppBeanScopes.kt
@@ -2,7 +2,6 @@ package vip.yazilim.basics.bean_scopes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import vip.yazilim.basics.component_scan.ctx1.AppComponentScanCtx1
 
 @SpringBootApplication
 class AppBeanScopes
---
+++ 03-spring-boot-thymeleaf/04-thymeleaf-crud/src/main/kotlin/vip/yazilim/thymeleaf/crud/controller/AboutController.kt
@@ -0,0 +1,22 @@
+package vip.yazilim.thymeleaf.crud.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+/**
+ *
+ * @author maemresen - maemresen@yazilim.vip
+ * 27.12.2020
+ */
+@Controller
+@RequestMapping("/about")
+class AboutController {
+
+    @GetMapping("")
+    fun about(): String {
+        return "about"
+    }
+
+}
